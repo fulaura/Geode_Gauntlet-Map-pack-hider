a940a3b6b334474c799498a0afef37f7830543f5
fulaura <bekzatashirbek2@gmail.com>
2026-08-06 17:07:12 +0500

    Add in-game toggle UI button to Gauntlets and Map Packs screens

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,35 @@ class $modify(MyGauntletSelectLayer, GauntletSelectLayer) {
                 filterDictionary(glm->m_savedGauntlets);
             }
         }
-        return GauntletSelectLayer::init(unused);
+        if (!GauntletSelectLayer::init(unused)) return false;
+
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        auto menu = CCMenu::create();
+        menu->setPosition({0, 0});
+        menu->setID("hide-completed-menu"_spr);
+        this->addChild(menu, 100);
+
+        auto offSpr = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
+        auto onSpr = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
+        if (offSpr && onSpr) {
+            offSpr->setScale(0.8f);
+            onSpr->setScale(0.8f);
+
+            auto toggler = CCMenuItemToggler::create(offSpr, onSpr, this, menu_selector(MyGauntletSelectLayer::onToggleHideCompleted));
+            bool isHiding = Mod::get()->getSettingValue<bool>("hide-completed");
+            toggler->toggle(isHiding);
+            toggler->setPosition({winSize.width - 30.0f, 30.0f});
+            toggler->setID("hide-completed-toggler"_spr);
+            menu->addChild(toggler);
+
+            auto label = CCLabelBMFont::create("Hide Done", "bigFont.fnt");
+            label->setScale(0.35f);
+            label->setPosition({winSize.width - 75.0f, 30.0f});
+            label->setID("hide-completed-label"_spr);
+            this->addChild(label, 100);
+        }
+
+        return true;
     }
 
     void setupGauntlets() {
@@ -63,9 +91,49 @@ class $modify(MyGauntletSelectLayer, GauntletSelectLayer) {
         }
         GauntletSelectLayer::setupGauntlets();
     }
+
+    void onToggleHideCompleted(CCObject* sender) {
+        bool current = Mod::get()->getSettingValue<bool>("hide-completed");
+        Mod::get()->setSettingValue("hide-completed", !current);
+        CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.4f, GauntletSelectLayer::scene(0)));
+    }
 };
 
 class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
+    bool init(GJSearchObject* object) {
+        if (!LevelBrowserLayer::init(object)) return false;
+
+        if (object && object->m_searchType == SearchType::MapPack) {
+            auto winSize = CCDirector::sharedDirector()->getWinSize();
+            auto menu = CCMenu::create();
+            menu->setPosition({0, 0});
+            menu->setID("hide-completed-menu"_spr);
+            this->addChild(menu, 100);
+
+            auto offSpr = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
+            auto onSpr = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
+            if (offSpr && onSpr) {
+                offSpr->setScale(0.8f);
+                onSpr->setScale(0.8f);
+
+                auto toggler = CCMenuItemToggler::create(offSpr, onSpr, this, menu_selector(MyLevelBrowserLayer::onToggleHideCompleted));
+                bool isHiding = Mod::get()->getSettingValue<bool>("hide-completed");
+                toggler->toggle(isHiding);
+                toggler->setPosition({winSize.width - 30.0f, 30.0f});
+                toggler->setID("hide-completed-toggler"_spr);
+                menu->addChild(toggler);
+
+                auto label = CCLabelBMFont::create("Hide Done", "bigFont.fnt");
+                label->setScale(0.35f);
+                label->setPosition({winSize.width - 75.0f, 30.0f});
+                label->setID("hide-completed-label"_spr);
+                this->addChild(label, 100);
+            }
+        }
+
+        return true;
+    }
+
     void setupLevelBrowser(cocos2d::CCArray* items) {
         if (Mod::get()->getSettingValue<bool>("hide-completed") && items) {
             auto toRemove = CCArray::create();
@@ -98,5 +166,13 @@ class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
             }
         }
         LevelBrowserLayer::loadLevelsFinished(levels, key, type);
+    }
+
+    void onToggleHideCompleted(CCObject* sender) {
+        bool current = Mod::get()->getSettingValue<bool>("hide-completed");
+        Mod::get()->setSettingValue("hide-completed", !current);
+        if (m_searchObject) {
+            this->loadPage(m_searchObject);
+        }
     }
 };
